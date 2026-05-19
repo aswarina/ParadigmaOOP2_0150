5b4a600a2a4cbd9efc9cb63b7059079a692864c6
aswarina <lyxsyyawa@gmail.com>
2026-05-19 19:40:18 +0700

    memanggil objek di main

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -32,10 +32,15 @@ public:
 
 class budi : public pekerja, public pelajar {
 public:
-    budi(int pUmur) :
-        pekerja(pUmur),
-        pelajar(pUmur)
-    {
-        cout << "Budi dibuat\n" << endl;
-    }
+    budi(int pUmur): 
+    orang(pUmur), 
+    pekerja(pUmur), 
+    pelajar(pUmur) {
+    cout << "Budi dibuat\n" << endl;
+}
 };
+
+int main(){
+    budi a(12);
+    return 0;
+}
